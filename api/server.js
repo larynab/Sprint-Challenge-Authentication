6a68ea417f1d3786f37b6c7e758510f6c6026acf
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-05 09:02:42 -0700

    token-created-signed-authenticated-register-login-get-jokes-success-implementing-comments-and-further-analysis

--- api/server.js
+++ api/server.js
@@ -1,26 +1,44 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-
-// const configureRoutes = require('../config/routes.js');
-// const authRouter = require('../auth/auth-router.js');
-// const usersRouter = require('../users/users-router.js');
-// const restricted = require('../auth/restricted-middleware.js');
-
-
+//server module
+const express = require("express");
+//cross-origin-data
+const cors = require("cors");
+//security
+const helmet = require("helmet");
+//axios is used for offsite data
+const axios = require("axios");
+//routers
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const restricted = require("../auth/restricted-middleware.js");
+//server
 const server = express();
-
+//middleware
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+//routing
+server.use("/api/auth", authRouter);
+server.use("/api/users", restricted, usersRouter);
+//Server is accessing api offsite using getJokes function
+server.get("/api/jokes", restricted, getJokes);
+//function used in API jokes
+function getJokes(req, res) {
+  const requestOptions = {
+    headers: { accept: "application/json" }
+  };
 
-// server.use('/api/auth', authRouter);
-// server.use('/api/users', restricted, usersRouter);
-
-// configureRoutes(server);
-
-server.get('/', (req, res) => {
-    res.send("It's alive!");
-  });
-
+  axios
+    .get("https://icanhazdadjoke.com/search", requestOptions)
+    .then(response => {
+      res.status(200).json(response.data.results);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
+    });
+}
+//root of server
+server.get("/", (req, res) => {
+  res.send("It's alive!");
+});
+//export!
 module.exports = server;
